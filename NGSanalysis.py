da392b699be4ebc4915bd62eb1072e7ac70aee13
soundslikealloy <eloi.mrp@gmail.com>
2023-10-20 12:09:18 -0500

    Rename

--- NGSanalysis.py
+++ NGSanalysis.py
@@ -10,6 +10,8 @@ from pathlib import Path
 from glob import glob
 import subprocess
 import re
+import argparse
+import os
 
 from pydna.amplify import pcr
 from Bio import AlignIO, SeqIO
@@ -26,6 +28,17 @@ import math
 import warnings
 warnings.filterwarnings("ignore")
 
+# Create a parser object
+parser = argparse.ArgumentParser(description = 'In-silico NGS analysis for specific set of primers and sequencing region (e.g., 16S rRNA, ITS region...)')
+parser.add_argument('-unique', dest = 'unique_amplicons', default = False, action = 'store_true')
+parser.add_argument('-nofig', dest = 'noFigure', default = False, action = 'store_true')
+parser.add_argument('-onlyfig', dest = 'figureOnly', default = False, action = 'store_true')
+
+args = parser.parse_args()
+unique_amplicons = args.unique_amplicons
+noFigure = args.noFigure
+figureOnly = args.figureOnly
+    
 # Directories definition
 root_dir = Path(__file__).resolve().parent.parent
 
@@ -54,11 +67,10 @@ featureRead.close()
 local_count = fwd.count('A') + fwd.count('T') + fwd.count('C') + fwd.count('G') # Only count ATCG nucleotides
 
 # Unique amplicons
-unique_amplicons = 0                                                            # Unique amplicons [0/1]
-if unique_amplicons == 1: dAmpl16S = {}                                         # Dictionary with amplicon 16S
+if unique_amplicons is True: dAmpl16S = {}                                         # Dictionary with amplicon 16S
 
 # FASTA file creation
-if unique_amplicons == 1:
+if unique_amplicons is True:
     fastafilename = 'out_fasta/fa_unique (' + str(time) + ').txt'
     fastafilealignmentname = 'out_fasta/fa_unique_alignment (' + str(time) + ').fa'
     fastafilealignmentsortname = 'out_fasta/fa_unique_alignment_sort (' + str(time) + ').fa'
@@ -72,11 +84,11 @@ fastafile = open(fastafilename, 'a')
 print('>> Getting amplicons from (.gb):')
 if len(glob('in_gb\*.gb')) == 0: print(' > No GenBank (Standard) files found.')
 for file in glob('in_gb\*.gb'):
-    if unique_amplicons == 1: dAmpl16S[file] = []
+    if unique_amplicons is True: dAmpl16S[file] = []
     iAmpl = 0
     i_ITS_location = []
     amplicon_len = []
-    if unique_amplicons == 1: iAmpl_unique = 0 
+    if unique_amplicons is True: iAmpl_unique = 0 
     id_strain = str(re.findall(r'\(.*?\)', str(file)))
     for gb_record in SeqIO.parse(open(file, 'r'), 'genbank'):
         if get_product[0] == 'spacer':
@@ -110,7 +122,7 @@ for file in glob('in_gb\*.gb'):
                         
                         # Write (unique) amplicons to FASTA file
                         amplicon_len.append(len(t_saved))
-                        if unique_amplicons == 1:
+                        if unique_amplicons is True:
                             if not dAmpl16S[file].count(t_saved):
                                 iAmpl_unique += 1
                                 dAmpl16S[file].append(t_saved)
@@ -143,7 +155,7 @@ for file in glob('in_gb\*.gb'):
                     
                     # Write (unique) amplicons to FASTA file
                     amplicon_len.append(len(t_saved))
-                    if unique_amplicons == 1:
+                    if unique_amplicons is True:
                         if not dAmpl16S[file].count(t_saved):
                             iAmpl_unique += 1
                             dAmpl16S[file].append(t_saved)
@@ -154,7 +166,7 @@ for file in glob('in_gb\*.gb'):
                         fastafile.write(str(t_saved) + '\n')
             
     # Printing results
-    if unique_amplicons == 0:
+    if unique_amplicons is False:
         if iAmpl == 1:
             copystr = 'copy'
         else:
@@ -172,9 +184,9 @@ for file in glob('in_gb\*.gb'):
 print('\n>> Getting amplicons from (.fa):')
 if len(glob('in_fasta\*.fa')) == 0: print(' > No FASTA files found.')
 for file in glob('in_fasta\*.fa'):
-    if unique_amplicons == 1: dAmpl16S[file] = []
+    if unique_amplicons is True: dAmpl16S[file] = []
     iAmpl = 0
-    if unique_amplicons == 1: iAmpl_unique = 0 
+    if unique_amplicons is True: iAmpl_unique = 0 
     id_strain = str(re.findall(r'\(.*?\)', str(file)))
     f = open(file, 'r')
     lines = f.readlines() 
@@ -207,7 +219,7 @@ for file in glob('in_fasta\*.fa'):
                     t_saved = t.seq
                     
                 # Write (unique) amplicons to FASTA file
-                if unique_amplicons == 1:
+                if unique_amplicons is True:
                     if not dAmpl16S[file].count(t_saved):
                         iAmpl_unique += 1
                         dAmpl16S[file].append(t_saved)
@@ -221,7 +233,7 @@ for file in glob('in_fasta\*.fa'):
         # Printing results
         fa_name = file.replace('in_fasta\\', '')
         fa_name = fa_name.replace('.fa', '')
-        if unique_amplicons == 0:
+        if unique_amplicons is False:
             if iAmpl == 1:
                 copystr = 'copy'
             else:
@@ -248,78 +260,88 @@ with fasta.read(fastafilealignmentname) as f:
     fasta.write(sorted(f), fastafilealignmentname)
 print('>> Alignment done!')
 
-# Alignment analysis (#mismatches)
-print('\n>> Plotting in progress...')
-file_misAnalysis = fastafilealignmentname
-filesave = file_misAnalysis.replace('out_fasta/', 'out_misAnalysis/')
-if unique_amplicons == 1:
-    filesave = filesave.replace('fa_unique_alignment', 'mismatchTable_unique')
-    figurefilename = 'out_misAnalysis/alignmismatches_unique (' + str(time) + ').png'
+if noFigure is False:
+    # Alignment analysis (#mismatches)
+    print('\n>> Plotting in progress...')
+    file_misAnalysis = fastafilealignmentname
+    filesave = file_misAnalysis.replace('out_fasta/', 'out_misAnalysis/')
+    if unique_amplicons is True:
+        filesave = filesave.replace('fa_unique_alignment', 'mismatchTable_unique')
+        figurefilename = 'out_misAnalysis/alignmismatches_unique (' + str(time) + ').png'
+    else:
+        filesave = filesave.replace('fa_alignment', 'mismatchTable')
+        figurefilename = 'out_misAnalysis/alignmismatches (' + str(time) + ').png'
+    filesave = filesave.replace('.fa', '.txt')
+    
+    
+    # Alignment dimensions
+    a = AlignIO.read(file_misAnalysis, 'fasta')
+    n_seq = a.__len__()                                                         # Num of sequences (rows)
+    alig_len = a.get_alignment_length()                                         # Max length of alignment (columns)
+    comb = np.array(list(combinations(np.arange(1, n_seq+1), 2)))
+    num_comb = math.comb(n_seq, 2)
+    indx1 = comb[:,0]
+    indx2 = comb[:,1]
+    
+    # Exporting sequences
+    mId = 'IDs'
+    mseq = np.zeros((1, alig_len))
+    for record in a:
+        iseq = list(record.seq)
+        mseq = np.vstack([mseq, iseq])
+        mId += ' ' + record.id
+    
+    mseq = mseq[1:n_seq+1, :]
+    mId = mId.split()[1:n_seq+1]
+    
+    # Matrix of mismatches
+    mMismatch = np.ones((n_seq, n_seq)) * (-1)
+    for iM in range(0, num_comb):
+        iIndx1 = indx1[iM] - 1
+        iIndx2 = indx2[iM] - 1        
+        mseq1 = mseq[iIndx1]
+        mseq2 = mseq[iIndx2]
+        mismatch_arr = np.compare_chararrays(mseq1, mseq2, "!=", rstrip = True)
+        iMismatch = mismatch_arr.sum()
+        mMismatch[iIndx2, iIndx1] = iMismatch
+    
+    # Save results
+    np.savetxt(filesave, mMismatch, fmt='%.0f')
+    
+    # Plotting
+    nrows = n_seq
+    ncols = n_seq
+    nTicks = np.arange(0.5, n_seq+0.5)
+    Z = mMismatch
+    x = np.arange(ncols + 1)
+    y = np.flip(np.arange(nrows + 1))
+    cmap = colors.LinearSegmentedColormap.from_list('', ['blue', 'yellow'])
+    cmap.set_under('black')
+    mX, mY = np.meshgrid(x, y)
+    
+    fig, ax = plt.subplots()
+    plt.plot(mX, mY, c='k', linewidth = '0.5')
+    plt.plot(np.transpose(mX), np.transpose(mY), c='k', linewidth = '0.5')
+    colormesh = ax.pcolormesh(x, y, Z, cmap = cmap, vmin = 0, vmax = 10)        # Z.min() \\ Z.max()
+    cbar = fig.colorbar(colormesh, label = '# mismatches')
+    plt.xticks(nTicks, mId, rotation=90)
+    plt.yticks(nTicks, np.flip(mId))
+    plt.rcParams.update({'font.size': 45})
+    
+    # Plot size
+    fig.set_figheight(n_seq)
+    fig.set_figwidth(n_seq+10)
+    
+    # Save plot
+    fig.savefig(figurefilename)
+    
+    print('>> Plotting done!')
 else:
-    filesave = filesave.replace('fa_alignment', 'mismatchTable')
-    figurefilename = 'out_misAnalysis/alignmismatches (' + str(time) + ').png'
-filesave = filesave.replace('.fa', '.txt')
-
-
-# Alignment dimensions
-a = AlignIO.read(file_misAnalysis, 'fasta')
-n_seq = a.__len__()                                                         # Num of sequences (rows)
-alig_len = a.get_alignment_length()                                         # Max length of alignment (columns)
-comb = np.array(list(combinations(np.arange(1, n_seq+1), 2)))
-num_comb = math.comb(n_seq, 2)
-indx1 = comb[:,0]
-indx2 = comb[:,1]
-
-# Exporting sequences
-mId = 'IDs'
-mseq = np.zeros((1, alig_len))
-for record in a:
-    iseq = list(record.seq)
-    mseq = np.vstack([mseq, iseq])
-    mId += ' ' + record.id
-
-mseq = mseq[1:n_seq+1, :]
-mId = mId.split()[1:n_seq+1]
-
-# Matrix of mismatches
-mMismatch = np.ones((n_seq, n_seq)) * (-1)
-for iM in range(0, num_comb):
-    iIndx1 = indx1[iM] - 1
-    iIndx2 = indx2[iM] - 1        
-    mseq1 = mseq[iIndx1]
-    mseq2 = mseq[iIndx2]
-    mismatch_arr = np.compare_chararrays(mseq1, mseq2, "!=", rstrip = True)
-    iMismatch = mismatch_arr.sum()
-    mMismatch[iIndx2, iIndx1] = iMismatch
-
-# Save results
-np.savetxt(filesave, mMismatch, fmt='%.0f')
-
-# Plotting
-nrows = n_seq
-ncols = n_seq
-nTicks = np.arange(0.5, n_seq+0.5)
-Z = mMismatch
-x = np.arange(ncols + 1)
-y = np.flip(np.arange(nrows + 1))
-cmap = colors.LinearSegmentedColormap.from_list('', ['blue', 'yellow'])
-cmap.set_under('black')
-mX, mY = np.meshgrid(x, y)
-
-fig, ax = plt.subplots()
-plt.plot(mX, mY, c='k', linewidth = '0.5')
-plt.plot(np.transpose(mX), np.transpose(mY), c='k', linewidth = '0.5')
-colormesh = ax.pcolormesh(x, y, Z, cmap = cmap, vmin = 0, vmax = 10)        # Z.min() \\ Z.max()
-cbar = fig.colorbar(colormesh, label = '# mismatches')
-plt.xticks(nTicks, mId, rotation=90)
-plt.yticks(nTicks, np.flip(mId))
-plt.rcParams.update({'font.size': 45})
-
-# Plot size
-fig.set_figheight(n_seq)
-fig.set_figwidth(n_seq+10)
-
-# Save plot
-fig.savefig(figurefilename)
-
-print('>> Plotting done!')
+    print('\n>> No Figure.')
+    
+if figureOnly is True:
+    # Delete all FASTA file
+    print('\n>> Only Figure was saved.')
+    os.remove(fastafilename)
+    os.remove(fastafilealignmentname)
+    os.remove(filesave)
